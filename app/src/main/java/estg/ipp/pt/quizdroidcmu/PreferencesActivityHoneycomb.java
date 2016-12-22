e887220fd61abca42c1a3237a7326ed1ea52c837
vsctiago <vsctiago@yahoo.com>
2016-12-22 11:39:25 +0000

    Preferences stuff

---
+++ app/src/main/java/estg/ipp/pt/quizdroidcmu/PreferencesActivityHoneycomb.java
@@ -0,0 +1,14 @@
+package estg.ipp.pt.quizdroidcmu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreferencesActivityHoneycomb extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PreferencesFragment()).commit();
+    }
+}
---
+++ app/src/main/java/estg/ipp/pt/quizdroidcmu/PreferencesFragment.java
@@ -0,0 +1,20 @@
+package estg.ipp.pt.quizdroidcmu;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PreferencesFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+    }
+}
